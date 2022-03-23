dd5e57bee1ed5412b4469afaf69244ef8186687a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 00:23:08 -0500

    se instala Firebase y se crea data de configuracion firebase

---
+++ src/data/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD05bEESfY-0YvHG81qRTGm6X91kycwMnE",
+    authDomain: "fir-tutorial-e6385.firebaseapp.com",
+    projectId: "fir-tutorial-e6385",
+    storageBucket: "fir-tutorial-e6385.appspot.com",
+    messagingSenderId: "857596999329",
+    appId: "1:857596999329:web:c0ed8f286401b27cc6a2e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
