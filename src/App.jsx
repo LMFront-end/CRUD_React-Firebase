0d146216bcf4190a8f99bfc409e2189d88dcaad4
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-23 00:55:07 -0500

    se instala Firebase y se crea data de configuracion firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState, useEffect } from "react"
+
+//firebase
+import {db} from "../src/data/firebase-config";
+import {collection,getDocs} from "firebase/firestore";
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+
+
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+
+        const data = await getDocs(usersCollectionRef);
+        setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})),);
+    }
+
+    getUsers()
+
+  }, [])
   
   return (
     <div className="App">
-      <h1>Hola Mundo</h1>
+      {users.map((user) => {
+        return <div>
+          {" "}
+          <h1 apiKey={user.id}>Name: {user.name}</h1>
+          <h1 apiKey={user.id}>Age: {user.age}</h1>
+        </div>
+      })}
     </div>
   )
 }
